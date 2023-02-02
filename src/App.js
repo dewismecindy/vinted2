40dcc985ace8f5ee43cc332ee7028047164d5ea3
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-02-02 01:15:30 +0100

    Routes Home+Offer+ create Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+/* Import packages */
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+/* Import pages */
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
+/* Import des components */
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/offer/:id" element={<Offer />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
