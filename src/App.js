662e590c1f06124b72c18a10cf5ec09b316ef081
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-02-02 20:58:00 +0100

    create form signUp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 /* Import packages */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cookies from "js-cookie";
 /* Import pages */
 import Home from "./pages/Home";
 import Offer from "./pages/Offer";
@@ -10,14 +11,21 @@ import Login from "./pages/Login";
 import Header from "./components/Header";
 
 function App() {
+  const handleToken = (token) => {
+    if (token) {
+      Cookies.set("userToken", token, { expires: 7 });
+    } else {
+      Cookies.remove("userToken");
+    }
+  };
   return (
     <Router>
-      <Header />
+      <Header handleToken={handleToken} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/offer/:id" element={<Offer />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup handleToken={handleToken} />} />
+        <Route path="/login" element={<Login handleToken={handleToken} />} />
       </Routes>
     </Router>
   );
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,14 @@
-const Signup = () => {
-  return <div>Signup</div>;
+const Signup = (handleToken) => {
+  return (
+    <form className="signup-container">
+      <h1>S'inscrire</h1>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+      <input type="checkbox" />
+      <input type="submit" value="S'inscrire" />
+    </form>
+  );
 };
 
 export default Signup;
