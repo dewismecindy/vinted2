e46de35dbd5dd434beecb5e94381b8a4db2d96dd
Cindewi <112582932+dewismecindy@users.noreply.github.com>
2023-02-02 21:54:39 +0100

    cookie done

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,59 @@
-const Signup = (handleToken) => {
+import { useState } from "react";
+import axios from "axios";
+
+const Signup = ({ handleToken }) => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [newsLetter, setNewsLetter] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://lereacteur-vinted-api.herokuapp.com/user/signup",
+        { email, username, password, newsletter: newsLetter }
+      );
+      /*  console.log(response.data); */
+      handleToken(response.data.token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <form className="signup-container">
+    <form className="signup-container" onSubmit={handleSubmit}>
       <h1>S'inscrire</h1>
-      <input type="text" placeholder="email" />
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
-      <input type="checkbox" />
+      <input
+        type="text"
+        placeholder="email"
+        value={email}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <input
+        type="checkbox"
+        checked={newsLetter}
+        onChange={() => {
+          setNewsLetter(!newsLetter);
+        }}
+      />
       <input type="submit" value="S'inscrire" />
     </form>
   );
